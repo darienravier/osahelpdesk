fa483020b55fa12e5d2a84fa94e8c9e213e092f3
Czar John Demafeliz <cjdemafeliz@gmail.com>
2018-01-04 17:49:52 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require('vendor/autoload.php');
+
+$f3 = Base::instance();										
+
+$f3->config('app/config.ini');
+if (file_exists('app/server.ini')) $f3->config('app/server.ini');
+$f3->config('app/routes.ini');
+
+$f3->run();
